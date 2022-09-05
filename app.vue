edaab247f4030b88a02dd2306ec9a95f81d0ac9a
Legaldesk14 <legaldesk14@Legalde14anders.localdomain>
2022-09-05 15:45:30 +0200

    Various pages added

--- app.vue
+++ app.vue
@@ -7,3 +7,13 @@
   <NuxtPage />
   <TheFooter />
 </template>
+
+<style>
+  body{
+    margin: 0;
+    background-color:#f7f7f7;
+    font-family:Open Sans, Helvetica Neue, Helvetica, Arial, sans-serif;
+    line-height:1.3;
+    width:100%
+  }
+</style>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,9 +1,24 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Component: TheFooter
-  </div>
+  <footer class="site-footer">
+    <p>Made by 
+      <a href="https://twitter.com/oaktoade" title="Follow @oaktoade  on Twitter" target="_blank">@oaktoade</a> &amp; 
+      <a href="https://twitter.com/farvig" title="Follow @favig  on Twitter" target="_blank">@ƒarvig</a>
+    </p>
+  </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.site-footer {
+    background: #337592;
+    background: linear-gradient(135deg, #337592 0%, #7089ca 44%, #709fb3 100%);
+    color: #f7f7f7;
+    margin-top: 44px;
+    padding: 3.75rem 0 4rem;
+    text-align: center;
+}
+.site-footer a {
+    color: #f7f7f7;
+}
+</style>
