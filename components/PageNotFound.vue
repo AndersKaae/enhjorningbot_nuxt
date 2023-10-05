e021e41c1e68745215643062406d36c74f5ef20e
Anders Lyager Kaae <anders.kaae@gmail.com>
2023-10-05 20:48:13 +0200

    404 page

---
+++ components/PageNotFound.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <h1>Page not found...</h1>
+        <img src="../assets/404.png" alt="404"/>
+    </div>
+</template>
+
+<style scoped>
+    h1{
+        font-size: 3rem;
+    }
+    img{
+        width: 100%;
+        max-width: 400px;
+    }
+
+    .container{
+        text-align: center;
+    }
+</style>
---
+++ error.vue
@@ -0,0 +1,5 @@
+<template>
+    <Navbar></Navbar>
+    <PageNotFound></PageNotFound>
+    <Footer></Footer>
+</template>
