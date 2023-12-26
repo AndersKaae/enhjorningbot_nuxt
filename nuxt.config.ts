28cc336dddccd36909491a50b086c51fd9065503
Anders Lyager Kaae <anders.kaae@gmail.com>
2023-12-26 09:53:29 +0100

    Gtag manager bug fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-simple-sitemap'],
+  modules: ['nuxt-simple-sitemap','nuxt-gtag'],
   sitemap: {
     // automatically chunk into multiple sitemaps
     sitemaps: true,
@@ -17,5 +17,10 @@ export default defineNuxtConfig({
     public: {
       gtagId: 'GTM-KJXRVZZP',
     }
+  },
+  // Google Analytics configuration
+  gtag: {
+    id: 'GTM-KJXRVZZP', // Your Google Analytics ID
+    // additional gtag configurations if needed
   }
 })  
