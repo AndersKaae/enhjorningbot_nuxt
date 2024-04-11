1a59305eeeda19253a11b5a193200de3a84f0d7c
Anders Lyager Kaae <anders.kaae@gmail.com>
2024-04-11 07:40:30 +0200

    follw button

--- components/company/BasicInfo.vue
+++ components/company/BasicInfo.vue
@@ -1,41 +1,51 @@
 <template>
-    <div v-if="generalInfo != ''" class="info-box">
-        <h1 class="company-name">{{ generalInfo.name }}</h1>
-        <span class="company-info-item">CVR-no.: <strong>{{ generalInfo.cvr }}</strong></span>
-        <span class="company-info-item">Founded: <strong>{{ generalInfo.start.split(' ')[0] }}</strong></span>
-        <span class="company-info-item">Industry: <strong>{{ generalInfo.branchekode }}</strong></span>
-        <template v-if="generalInfo.Website != null">
-            <span class="company-info-item">Website: <strong>{{ generalInfo.Website }}</strong></span>
-        </template>
-        <span class="company-info-item">Address: <strong>{{ generalInfo.adress }}</strong></span>
+  <div v-if="generalInfo != ''" class="info-box">
+    <div class="headline">
+      <h1 class="company-name">{{ generalInfo.name }}</h1>
+      <CompanyFollowButton></CompanyFollowButton>
     </div>
+    <span class="company-info-item">CVR-no.: <strong>{{ generalInfo.cvr }}</strong></span>
+    <span class="company-info-item">Founded: <strong>{{ generalInfo.start.split(' ')[0] }}</strong></span>
+    <span class="company-info-item">Industry: <strong>{{ generalInfo.branchekode }}</strong></span>
+    <template v-if="generalInfo.Website != null">
+      <span class="company-info-item">Website: <strong>{{ generalInfo.Website }}</strong></span>
+    </template>
+    <span class="company-info-item">Address: <strong>{{ generalInfo.adress }}</strong></span>
+  </div>
 </template>
 
 <style scoped>
+.headline {
+  display: flex;
+  align-items: center; /* This vertically centers the items in the container */
+  justify-content: space-between; /* This spreads the items out, with max space between. Use 'flex-start' if you want them close to each other */
+  margin-bottom: 1rem; /* Optional: Adjusts space below the headline */
+}
+
 .company-name {
-    font-size: 3rem;
-    margin-bottom: 1rem;
+  font-size: 3rem;
+  margin-bottom: 1rem;
 }
 
 .company-info-item {
-    margin-right: 1em;
+  margin-right: 1em;
 }
 
-@media (max-width: 640px) {
-    .company-name{
+    @media (max-width: 640px) {
+      .company-name{
         font-size: 2rem;
-    }
+      }
 
-    .company-info-item {
+      .company-info-item {
         display: block;
         margin-bottom: .5rem;
         margin-right: 0;
+      }
     }
-}
 </style>
 
 <script setup>
-const props = defineProps(["generalInfo"]);
+  const props = defineProps(["generalInfo"]);
 let generalInfo = ref(props.generalInfo);
 
 
---
+++ components/company/FollowButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="follow-button" @click="followUser">
+    <span v-if="isLoading">
+      <font-awesome-icon :icon="['fas', 'spinner']" spin-pulse />
+    </span>
+    <span v-if="!isLoading">
+      <span v-if="isFollowing">Unfollow</span>
+      <span v-else>Follow Company</span>
+    </span>
+  </div> 
+</template>
+
+<script setup>
+  const config = useRuntimeConfig()
+  var isLoading = ref(false);
+  var isFollowing = ref(false);
+  onBeforeMount(() => {
+    if (!config.public.isLoggedin) {
+      isFollowing.value = false;
+      console.log(config)
+    }
+    else
+    {
+    setTimeout(() => {
+      isLoading.value = false;
+      isFollowing.value = true;
+    }, 2000);
+  }});
+</script>
+
+<style scoped>
+.follow-button {
+  display: flex; /* Turns the button into a flex container */
+  justify-content: center; /* Centers children horizontally in the container */
+  align-items: center; /* Centers children vertically in the container */
+  background-color: #A020F0;
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+  height: 50px; /* Ensures the button has a fixed height */
+  width: fit-content; /* Optional: Adjusts width based on content size */
+  border: none; /* Optional: Removes border if any */
+  min-width: 150px; /* Optional: Sets a minimum width */
+  margin-left: 20px;
+}
+
+.follow-button:hover {
+  background-color: #bd53ff;
+}
+</style>
+
